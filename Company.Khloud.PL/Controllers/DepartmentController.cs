1afd5e5561c4888b6db7b040fa7d7cfaa9d13355
HP <HP@ENGKHLOUD>
2025-08-29 12:37:49 +0300

    Create Create (Action - View)

--- Company.Khloud.PL/Controllers/DepartmentController.cs
+++ Company.Khloud.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Khloud.BLL.Interfaces;
 using Company.Khloud.BLL.Repositories;
+using Company.Khloud.DAL.Models;
+using Company.Khloud.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Khloud.PL.Controllers
@@ -20,6 +22,36 @@ namespace Company.Khloud.PL.Controllers
             //DepartmentRepository departmentRepository = new DepartmentRepository();
            var departments= _departmentRepository.GetAll();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) //Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+
+                };
+
+                var Count = _departmentRepository.Add(department);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+
+            }
+            return View(model);
         }
     }
 }
---
+++ Company.Khloud.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Khloud.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage ="Code Is Required !")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "CreateAt Is Required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
