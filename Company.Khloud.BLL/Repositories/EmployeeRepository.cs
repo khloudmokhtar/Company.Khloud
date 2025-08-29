eeb38b877005f27bbfbedd5d75bc458913bc2964
HP <HP@ENGKHLOUD>
2025-08-29 19:09:56 +0300

    Implementation Of BLL For Employee Service Before Generic

---
+++ Company.Khloud.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,52 @@
+﻿using Company.Khloud.BLL.Interfaces;
+using Company.Khloud.DAL.Data.Contexts;
+using Company.Khloud.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Khloud.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+           return _context.Employees.ToList();
+        }
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+      
+
+    
+
+    
+    }
+}
